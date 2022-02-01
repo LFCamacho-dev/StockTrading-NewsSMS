5acfd193f84a4b6af521d6fc6e02ebb8979ce3df
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-02-01 01:14:42 -0600

    sending sms when stock increases/decreases by 5%, including the Stock name, the percentage and a news article related to the stock's company

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import random
 import requests
 import pandas
+from twilio.rest import Client
 
 stock_api_key = os.environ.get("STOCK_API_KEY")
 STOCK = "TSLA"
@@ -13,9 +15,6 @@ stock_params = {
     "apikey": stock_api_key,
 }
 
-
-# STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 def get_stocks():
     response = requests.get(f"https://www.alphavantage.co/query", params=stock_params)
@@ -36,12 +35,11 @@ def get_stocks():
 
     if closing_difference >= 5:
         print(f"The TSLA stock went up {closing_difference}%")
+        send_sms("🔺", closing_difference)
     elif closing_difference <= -5:
         print(f"The TSLA stock went down {closing_difference}%")
+        send_sms("🔻", closing_difference)
 
-
-# STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 news_api_key = os.environ.get("NEWS_API_KEY")
 news_params = {
@@ -51,43 +49,32 @@ news_params = {
     "apiKey": news_api_key,
 }
 
+api_key = os.environ.get("APIKEY")
+account_sid = os.environ.get("ACC_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-def get_news():
+
+def send_sms(tri, dif):
+
     response = requests.get(f"https://newsapi.org/v2/top-headlines", params=news_params)
     response.raise_for_status()
     news_data = response.json()
-    for article in range(3):
-        print(f"{news_data['articles'][article]}\n")
+    # for article in range(3):
+    #     print(f"{news_data['articles'][article]}\n")
+    news_article = news_data['articles'][random.randint(0, 2)]
+    article_title = news_article["title"]
+    article_description = news_article["description"]
+
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to=os.environ.get("TO_TEL"),
+        from_=os.environ.get("FROM_TEL"),
+        body=f"{STOCK}: {tri}{dif}%\n"
+             f"Headline: {article_title}\n"
+             f"Brief: {article_description}\n"
+    )
+
+    print(f"the ID is: {message.sid}, and the status is: {message.status}")
 
 
-get_news()
-
-
-
-
-
-
-
-
-
-# STEP 3: Use https://www.twilio.com
-# Send a separate message with the percentage change and each article's title and description to your phone number.
-
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors 
-are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, 
-near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors 
-are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, 
-near the height of the coronavirus market crash.
-"""
-
-
-
+get_stocks()
