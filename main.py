67bbc8bd5fc721342375cf2ead97bd0bd1d96b64
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-31 23:44:55 -0600

    added news api, gets the 3 most recent news articles about Tesla

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ stock_api_key = os.environ.get("STOCK_API_KEY")
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-# STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
@@ -16,32 +13,62 @@ stock_params = {
     "apikey": stock_api_key,
 }
 
-response = requests.get(f"https://www.alphavantage.co/query", params=stock_params)
-response.raise_for_status()
-stock_data = response.json()
-# print(stock_data)
 
-stock_data_df = pandas.DataFrame(stock_data)
-df_sliced = stock_data_df["Time Series (Daily)"].iloc[5:7]
+# STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-stock_dif = []
-for day_data in df_sliced:
-    stock_dif.append(float(day_data["4. close"]))
-    print(day_data["4. close"])
+def get_stocks():
+    response = requests.get(f"https://www.alphavantage.co/query", params=stock_params)
+    response.raise_for_status()
+    stock_data = response.json()
+    # print(stock_data)
 
-closing_difference = round((stock_dif[0] - stock_dif[1]) / stock_dif[1] * 100, 2)
-print(closing_difference)
+    stock_data_df = pandas.DataFrame(stock_data)
+    df_sliced = stock_data_df["Time Series (Daily)"].iloc[5:7]
 
-if closing_difference >= 5:
-    print(f"Great News! the TSLA stock went up {closing_difference}%")
-elif closing_difference <= -5:
-    print(f"Great News! the TSLA stock went down {closing_difference}%")
+    stock_dif = []
+    for day_data in df_sliced:
+        stock_dif.append(float(day_data["4. close"]))
+        print(day_data["4. close"])
 
+    closing_difference = round((stock_dif[0] - stock_dif[1]) / stock_dif[1] * 100, 2)
+    print(closing_difference)
 
+    if closing_difference >= 5:
+        print(f"The TSLA stock went up {closing_difference}%")
+    elif closing_difference <= -5:
+        print(f"The TSLA stock went down {closing_difference}%")
 
 
 # STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+news_api_key = os.environ.get("NEWS_API_KEY")
+news_params = {
+    "qInTitle": COMPANY_NAME,
+    "q": "Tesla",
+    "sortBy": "publishedAt",
+    "apiKey": news_api_key,
+}
+
+
+def get_news():
+    response = requests.get(f"https://newsapi.org/v2/top-headlines", params=news_params)
+    response.raise_for_status()
+    news_data = response.json()
+    for article in range(3):
+        print(f"{news_data['articles'][article]}\n")
+
+
+get_news()
+
+
+
+
+
+
+
+
 
 # STEP 3: Use https://www.twilio.com
 # Send a separate message with the percentage change and each article's title and description to your phone number.
