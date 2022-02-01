7307b9334c72040b1045eedcb8ac9b95ef2165d5
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-31 21:04:42 -0600

    fetching data from alphavantage.co (stock market data), checking if Tesla stocks increased or decreased in the last 2 days. Notifying the user if the increase/decrease is bigger than 5%

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
+import os
 import requests
+import pandas
 
-
-
+stock_api_key = os.environ.get("STOCK_API_KEY")
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-## STEP 1: Use https://www.alphavantage.co
+# STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-## STEP 2: Use https://newsapi.org
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    # "interval": "60min",
+    "apikey": stock_api_key,
+}
+
+response = requests.get(f"https://www.alphavantage.co/query", params=stock_params)
+response.raise_for_status()
+stock_data = response.json()
+# print(stock_data)
+
+stock_data_df = pandas.DataFrame(stock_data)
+df_sliced = stock_data_df["Time Series (Daily)"].iloc[5:7]
+
+stock_dif = []
+for day_data in df_sliced:
+    stock_dif.append(float(day_data["4. close"]))
+    print(day_data["4. close"])
+
+closing_difference = round((stock_dif[0] - stock_dif[1]) / stock_dif[1] * 100, 2)
+print(closing_difference)
+
+if closing_difference >= 5:
+    print(f"Great News! the TSLA stock went up {closing_difference}%")
+elif closing_difference <= -5:
+    print(f"Great News! the TSLA stock went down {closing_difference}%")
+
+
+
+
+# STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# STEP 3: Use https://www.twilio.com
+# Send a separate message with the percentage change and each article's title and description to your phone number.
 
 
-#Optional: Format the SMS message like this: 
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
@@ -29,4 +61,6 @@ Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and 
 are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, 
 near the height of the coronavirus market crash.
 """
+
+
 
